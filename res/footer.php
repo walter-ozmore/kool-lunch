35ddd422bb5dbecb5e3b6657ca9fa2525b3e3f68
Walter Ozmore <walter.ozmore.327@gmail.com>
2023-05-21 23:09:07 -0500

    Added link

--- res/footer.php
+++ res/footer.php
@@ -1,5 +1,6 @@
 <footer class="footer">
   <!-- <p>Guild star info 501(c)(3) non profit coalition</p> -->
   <p>The Kool Lunches Program is a 501(3)(c) non-profit coalition</p>
+  <p>Website by <a href="https://www.everyoneandeverything.org/profile/">Walter Ozmore</a></p>
   <p id="msg" class="subtle" style="margin: 0em auto;"></p>
 </footer>
