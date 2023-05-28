f240c99a084aa8c1ea3730d31776bc49836a7304
Walter Ozmore <walter.ozmore.327@hotmail.com>
2023-05-28 12:26:19 -0500

    Removed SQL information

--- ajax/sign-up.php
+++ ajax/sign-up.php
@@ -1,20 +1,32 @@
 <?php
-  echo "Start\n";
-  // Set up database connection
-  $server = "localhost";
-  $username = "everying_lunch";
-  $password = "Ac6jNUH8W2eZpiN";
-  $database = "everying_koolLunches";
+  require_once realpath($_SERVER["DOCUMENT_ROOT"])."/res/lib.php";
 
-  $conn = new mysqli($server, $username, $password, $database);
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+  function formatPhoneNumber($phoneNumber) {
+
+    // Remove any non-numeric characters from the phone number
+    $phoneNumber = preg_replace('/[^0-9]/', '', $phoneNumber);
+
+    // Extract the area code, prefix, and line number
+    $areaCode = substr($phoneNumber, 0, 3);
+    $prefix = substr($phoneNumber, 3, 3);
+    $lineNumber = substr($phoneNumber, 6, 4);
+
+    $formattedNumber = "($areaCode) $prefix-$lineNumber";
+
+    if(
+      $formattedNumber === "() -" ||
+      strlen($formattedNumber) != strlen("(xxx) xxx-xxxx") ||
+      $formattedNumber === $phoneNumber
+    ) return $phoneNumber;
+
+    // Return the formatted phone number
+    return $formattedNumber;
   }
 
   // Get POST data and sanitize
   $data = json_decode($_POST["q"], true);
   $pickupDays = array_map('strtolower', array_map('trim', $data["general"]["days"]));
-  $location = $conn->real_escape_string(trim($data["general"]["location"]));
+  $location = $db_conn->real_escape_string(trim($data["general"]["location"]));
 
   // Insert form data into Form table
   $pickupMonday = in_array("monday", $pickupDays) ? 1 : 0;
@@ -25,38 +37,38 @@
 
   $query = "INSERT INTO Form (PickupMonday, PickupTuesday, PickupWednesday, PickupThursday, TimeSubmited, Location)
           VALUES ($pickupMonday, $pickupTuesday, $pickupWednesday, $pickupThursday, $timeSubmitted, '$location')";
-  if( !($result = $conn->query($query)) ) {
-    echo "$query\n$conn->error\n";
+  if( !($result = $db_conn->query($query)) ) {
+    echo "$query\n$db_conn->error\n";
     exit;
   }
-  $formId = $conn->insert_id;
+  $formId = $db_conn->insert_id;
 
   // Insert adult data into Individual table
   foreach ($data["adults"] as $adult) {
-    $name = $conn->real_escape_string(trim($adult["name"]));
-    $phoneNumber = $conn->real_escape_string(trim($adult["phoneNumber"]));
+    $name = $db_conn->real_escape_string(trim($adult["name"]));
+    $phoneNumber = formatPhoneNumber( $adult["phoneNumber"] );
+    $phoneNumber = $db_conn->real_escape_string(trim($phoneNumber));
     $isAdult = 1;
     $requestRemindAccess = 0;
 
     $query = "INSERT INTO Individual (IndividualName, PhoneNumber, IsAdult, FormId, RemindStatus)
             VALUES ('$name', '$phoneNumber', $isAdult, $formId, $requestRemindAccess)";
-    if( !($result = $conn->query($query)) ) echo "$query\n$conn->error\n";
+    if( !($result = $db_conn->query($query)) ) echo "$query\n$db_conn->error\n";
   }
 
   // Insert child data into Individual table
   foreach ($data["children"] as $child) {
-    $name = $conn->real_escape_string(trim($child["name"]));
+    $name = $db_conn->real_escape_string(trim($child["name"]));
     $isAdult = 0;
-    $allergies = $conn->real_escape_string(trim($child["allergies"]));
+    $allergies = $db_conn->real_escape_string(trim($child["allergies"]));
     $allowPhotos = (sizeof($child["allowPhotos"]) > 0)? 1 : 0;
 
 
     $query = "INSERT INTO Individual (IndividualName, IsAdult, FormId, Allergies, AllowPhotos)
             VALUES ('$name', $isAdult, $formId, '$allergies', $allowPhotos)";
-    if( !($result = $conn->query($query)) ) echo "$query\n$conn->error\n";
+    if( !($result = $db_conn->query($query)) ) echo "$query\n$db_conn->error\n";
   }
 
-  // Close database connection
-  $conn->close();
-  echo "Done";
+  // Close database db_connection
+  $db_conn->close();
 ?>
