d8d2b3cec49e97610b4a80e689d88525f10ac424
Walter Ozmore <walter.ozmore.327@gmail.com>
2024-01-07 15:03:57 -0600

    Added links to footer

--- res/footer.php
+++ res/footer.php
@@ -2,6 +2,13 @@
   <!-- <p>Guild star info 501(c)(3) non profit coalition</p> -->
   <p>The Kool Lunches Program is a 501(3)(c) non-profit coalition</p>
   <p>Website by <a href="https://walter-ozmore.dev/">Walter Ozmore</a> & Rayna Fetters</p>
+  <p class="footer-links">
+    <a href="/">Home</a>
+    <!-- <a href="/form">Sign Up</a> -->
+    <a href="/volunteer-sign-up">Volunteer</a>
+    <a href="/tracker">Tracker</a>
+    <a href="/admin">Admin</a>
+  </p>
 
   <!-- This is for debug messages -->
   <p id="debug-msg" class="subtle" style="margin: 0em auto;"></p>
