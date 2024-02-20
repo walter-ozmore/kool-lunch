f9f83704025822399c75828ae42d0f79f2f7e024
Walter Ozmore <walter.ozmore.327@gmail.com>
2024-02-20 07:42:50 -0600

    Removed console log

--- scripts/admin/volunteer-forms.js
+++ scripts/admin/volunteer-forms.js
@@ -3,7 +3,6 @@ $(document).ready(async function() {
 		let data = (await post("/ajax/admin.php", {
 			function: 1
 		})).data;
-    console.log(data);
 
 		let tableDiv = mktable(data, {
 			headerNames: tableHeaderNames,
