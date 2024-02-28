c7b4a4b42bf7083f96fa35578c8b76e36bb37985
Walter Ozmore <walter.ozmore.327@gmail.com>
2024-02-27 20:24:23 -0600

    Maybe shows data in the form?

--- scripts/admin/organizations.js
+++ scripts/admin/organizations.js
@@ -24,6 +24,24 @@ $(document).ready(async function() {
 			function: 4
 		})).data;
 
+    // Pull data out to be one layer
+    for(let row of data) {
+      if("mainContact" in row) {
+        let id = row.mainContact.individualID;
+        let name = row.mainContact.individualName;
+        delete row.mainContact;
+        row.mainContact = name;
+        row.mainContactID = id;
+      }
+      if("signupContact" in row) {
+        let id = row.signupContact.individualID;
+        let name = row.signupContact.individualName;
+        delete row.signupContact;
+        row.signupContact = name;
+        row.signupContactID = id;
+      }
+    }
+
 		let tableDiv = mktable(data, {
 			headerNames: tableHeaderNames,
 			triggers: tableTriggers,
